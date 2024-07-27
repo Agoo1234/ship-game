8d7d5a8616ea9abc19b2a21ebf6223ae5db661c5
Your Name <you@example.com>
2024-07-27 01:44:09 +0000

    Implemented a disconnect screen to display when the player is disconnected from the server.

--- public/game.js
+++ public/game.js
@@ -48,7 +48,9 @@ function connectWebSocket() {
 
     ws.onmessage = (event) => {
         const data = JSON.parse(event.data);
-        if (data.type === 'dead') {
+        if (data.type === 'disconnected') {
+            showDisconnectScreen();
+        } else if (data.type === 'dead') {
             showDeathScreen();
         } else if (data.type === 'gameState') {
             players = data.players;
@@ -239,6 +241,19 @@ function gameLoop() {
 
     ctx.save();
     ctx.translate(-camera.x, -camera.y);
+
+}
+
+function showDisconnectScreen() {
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    ctx.fillStyle = 'white';
+    ctx.font = '30px Arial';
+    ctx.textAlign = 'center';
+    ctx.fillText('Disconnected from server', canvas.width / 2, canvas.height / 2 - 20);
+    ctx.fillText('Please refresh the page to reconnect', canvas.width / 2, canvas.height / 2 + 20);
+}
 
     stars.forEach(star => drawStar(star));
     players.forEach(player => drawShip(player));
